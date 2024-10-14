d1e9ac5cee3b22cdfff3442e39c9f42d56d8cf09
Nagy Adam <nagy.adam@diak.szbi-pg.hu>
2024-10-14 09:22:24 +0200

    05-os feladat update

--- 05_random_szamok.py
+++ 05_random_szamok.py
@@ -1,4 +1,34 @@
+# import random
+
+# random_szam = random.randint(1, 4)
+# print(random_szam)
+
+"""
+1. Feladat
+Készíts egy rövid programot, amely 1 és 3 között generál véletlenszámot, majd összehasonlítja ezt a felhasználó
+által megadott, szintén ebbe a tartományba eső számmal! Az összehasonlítás eredményéről tájékoztassa a felhasználót!
+"""
 import random
 
-random_szam = random.randint(1, 4)
-print(random_szam)
+number = int(input("Gondoltam egy számra 1 és 3 között. Próbáld meg kitalálni:\n "))
+random_szam = random.randint(1, 3)
+
+if number == random_szam:
+    print("Eltaláltad!")
+elif number != random_szam:
+    print("Nem talált!")
+    print(f"A szám {random_szam} volt.")
+
+"""2. Feladat
+A program a pénzfeldobást modellezi. Kérdezze meg a felhasználótól a 
+választását (fej vagy írás), majd adjon tájékoztatást, hogy eltalálta-e!
+"""
+
+print("Fej, vagy írás?")
+valasz = input("1: Fej\n2: Írás\nVálasz: ")
+fejviras = random.randint(1, 2)
+
+if valasz == fejviras:
+    print("Helyes.")
+else:
+    print("Helytelen.")
